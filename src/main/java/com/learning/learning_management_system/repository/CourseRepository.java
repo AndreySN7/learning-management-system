cb38e181acd976c2945f0fed7929b310efddfbd7
AndreySN7 <andrey__n@mail.ru>
2026-09-16 16:28:13 +0300

    Интеграционные тесты (CourseIT)

--- src/main/java/com/learning/learning_management_system/repository/CourseRepository.java
+++ src/main/java/com/learning/learning_management_system/repository/CourseRepository.java
@@ -3,6 +3,8 @@ package com.learning.learning_management_system.repository;
 import com.learning.learning_management_system.entity.Course;
 import com.learning.learning_management_system.exception.EntityNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
@@ -10,4 +12,7 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
 		return findById(id)
 					.orElseThrow(() -> new EntityNotFoundException("Course not found"));
 	}
+
+	@Query(value = "select c.deleted from course c where id = :id", nativeQuery = true)
+	Boolean isDeletedById(@Param(value = "id") Long id);
 }
